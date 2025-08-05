557747cd3d83a2b518059a2dace1d462f4e88d5f
Dhruv Chaturvedi <dhruv@chaturvedi.email>
2025-08-05 13:25:46 +0530

    added status and activity from env

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const client = new Client()
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setStatus('dnd') // set the status to do not disturb
+    client.user.setActivity(process.env.ACTIVITY_MESSAGE || 'Whats good bruv', { type: 'WATCHING' }) // set the activity from env or default
+    console.log(`Status set to DND and activity set to "Bumping the server!"`) // log the status and activity in console
 
     const channel = await client.channels.fetch(process.env.BUMP_CHANNEL) // get the channel to send bumps
     const logChannel = await client.channels.fetch(process.env.LOG_CHANNEL) // get the channel to log bumps
-
+    if (!channel || !logChannel) {
+        console.error('Bump or log channel not found! Please check your .env file.')
+        return
+    }
+    console.log(`Bump channel and log channel fetched successfully.`) // log the channels fetched in console
+    
     async function bump() {
         await channel.sendSlash('302050872383242240', 'bump') // send the bump command in the channel
         console.log(`Bumped the server at ${new Date().toLocaleTimeString()}`) // log the bump time in console
